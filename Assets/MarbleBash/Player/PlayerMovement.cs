4adb681618c2e74db6b78b41a41224395a0102a1
Oliver Coates <oliver.gcoates@gmail.com>
2026-03-26 15:08:49 +1300

    Player can jump

--- Assets/MarbleBash/Player/PlayerMovement.cs
+++ Assets/MarbleBash/Player/PlayerMovement.cs
@@ -1,12 +1,13 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody _rb;
+    [Header("References:")]
     [SerializeField] private PlayerLook _playerLook;
+    private Rigidbody _rb;
 
-    [SerializeField] private float _speed;
 
     #region Input
     [Header("Input:")]
@@ -17,8 +18,27 @@ public class PlayerMovement : MonoBehaviour
         InputActionMap map = _inputActions.FindActionMap("Player");
 
         _moveAction = map.FindAction("Move");
+        map.FindAction("Jump").performed += AttemptJump;
     }
+
     #endregion
+
+
+    [Header("Settings:")]
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
+
+    [Header("State:")]
+    [SerializeField] private LayerMask _groundedLayerMask;
+    [SerializeField] private bool _isGrounded;
+    public bool isGrounded
+    {
+        get
+        {
+            return _isGrounded;
+        }
+    }
+
 
     private void Start()
     {
@@ -27,12 +47,41 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Update()
-    {   
+    {
+        _isGrounded = CheckIsGrounded();
+
+        MoveHorizontally();
+    }
+
+    private bool CheckIsGrounded()
+    {
+        float halfScale = transform.localScale.x / 2f;
+        Vector3 groundBoxPosition = transform.position + (Vector3.down * halfScale);
+        Vector3 goundBoxSize = new Vector3(halfScale * 0.85f, 0.05f, halfScale * 0.85f);
+
+        return Physics.CheckBox(groundBoxPosition, goundBoxSize, Quaternion.identity, _groundedLayerMask);
+    }
+
+    private void MoveHorizontally()
+    {
         Vector3 movementInput = _moveAction.ReadValue<Vector2>();
 
-        Vector3 forceThisFrame = _playerLook.yaw * movementInput.y + _playerLook.yawRight * movementInput.x;
+        Vector3 forceThisFrame = (_playerLook.yaw * movementInput.y) + _playerLook.yawRight * movementInput.x;
 
         _rb.AddForce(_speed * Time.deltaTime * forceThisFrame);
     }
 
+
+    private void AttemptJump(InputAction.CallbackContext context)
+    {
+        if (isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        _rb.AddForce(Vector3.up * _jumpForce, ForceMode.VelocityChange);
+    }
 }
