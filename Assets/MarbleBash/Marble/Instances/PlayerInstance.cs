19cdd2addd256a65fceac9c027fbaf183fefa7b1
Oliver Coates <oliver.gcoates@gmail.com>
2026-04-14 16:42:04 +1200

    Fixed damage rampup and damage calculations for collisions with marbles

--- Assets/MarbleBash/Marble/Instances/PlayerInstance.cs
+++ Assets/MarbleBash/Marble/Instances/PlayerInstance.cs
@@ -5,9 +5,6 @@ namespace MarbleBash
     [System.Serializable]
     public class PlayerInstance : Marble
     {
-        /// <summary>
-        /// Setup for player marble is easy, just pull data from the player singleton.
-        /// </summary>
         protected override void Setup()
         {
             _rigidbody = Player.rigidbody;
@@ -18,6 +15,5 @@ namespace MarbleBash
         {
             return Player.look.pitchForward;
         }
-
     }
 }
--- Assets/MarbleBash/Player/Combat/PlayerDamageManager.cs
+++ Assets/MarbleBash/Player/Combat/PlayerDamageManager.cs
@@ -14,85 +14,96 @@ namespace MarbleBash
             }
         }
 
-
+        /// <summary>
+        /// Handles a collision between the player and an enemy.
+        /// Calculated and applies damage to each entity based on their speed, direction & mass.
+        /// 
+        /// ### HOW DAMAGE IS CALCUALTED: ###
+        /// 
+        /// 'Aligned Velocity' is the magnitude of a marble's velocity, scaled by how much their velocity is pointing towards their target.
+        /// - For instance, a slight glancing blow does much less damage than a direct hit.
+        /// 
+        /// 'Raw Damage' is simply mass * aligned velocity.
+        ///  - The size and the speed of your marble affect the damage dealt,
+        ///  - And maximum damage is dealt when your velocity in pointing directly at the hit marble
+        /// 
+        /// The damage of the two marbles are then compared, if one marble's raw damage is greater than double the other marble, it 
+        /// deals 100% damage while the other marble's damage is ignored.
+        /// 
+        /// </summary>
+        /// <param name="collision"></param>
+        /// <param name="enemy"></param>
         private void HandleCollisionWithEnemy(Collision collision, EnemyInstance enemy)
         {
-            // Find velocity against each other:
-            Vector3 playerVelocity = Player.instance.cachedVelocity;
-            Vector3 enemyVelocity = enemy.cachedVelocity;
-            Debug.Log($"Player before: {Player.rigidbody.linearVelocity.magnitude:0.0}, After: {playerVelocity.magnitude:0.0}");
-            Debug.Log($"Enemy before: {enemy.rigidbody.linearVelocity.magnitude:0.0}, After: {enemyVelocity.magnitude:0.0}");
-
-            // Find direction between entities
-            Vector3 directionToEnemy = (enemy.transform.position - Player.transform.position).normalized;
-            Vector3 directionToPlayer = -directionToEnemy;
+            // Find the aliged velocity between the player and enemy:
+            float alignedVelocityPlayer = FindAlignedVelocity(Player.instance, enemy);
+            float alignedVelocityEnemy = FindAlignedVelocity(enemy, Player.instance);
 
             // Find the raw damage from this collision 
-            float rawPlayerDamage = FindDamageForCollision(Player.rigidbody, directionToEnemy, Player.rigidbody.mass, out float scaledPlayerVelocity);
-            float rawEnemyDamage = FindDamageForCollision(enemy.rigidbody, directionToPlayer, enemy.rigidbody.mass, out float scaledEnemyVelocity);
+            float rawPlayerDamage = FindRawDamageForCollision(Player.instance, Player.rigidbody.mass, alignedVelocityPlayer);
+            float rawEnemyDamage = FindRawDamageForCollision(enemy, enemy.rigidbody.mass, alignedVelocityEnemy);
 
             // Find damage scaling value:
             // A value of 1 means the player deals 100% damage and the enemy 0 
             // A value of 0 means both marbles deal 50% damage each 
             // A value of -1 means the enemy marble deals 100% and the player 0
-            float damageScaling = FindDamageScaling(scaledPlayerVelocity, scaledEnemyVelocity);
+            float damageScaling = FindDamageRampup(rawPlayerDamage, rawEnemyDamage);
 
-            Debug.Log($"Player: {scaledPlayerVelocity}, Enemy: {scaledEnemyVelocity}, Damage scaling: {damageScaling}");
+            Debug.Log($"Player: {rawPlayerDamage}, Enemy: {rawEnemyDamage}, Damage scaling: {damageScaling}");
 
         }
 
-        /// <summary>
-        /// Reconstructs the velocities of two bodies after a collision.
-        /// </summary>
-        static void FindIncidentVelocity(Collision collision, Rigidbody A, Rigidbody B, out Vector3 aVelocity, out Vector3 bVelocity)
+        private float FindAlignedVelocity(Marble primaryBody, Marble hitBody)
         {
-            Vector3 impulse = collision.impulse;
+            Vector3 velocity = primaryBody.cachedVelocity;
 
-            // Both participants of a collision see the same impulse, so we need to flip it for one of them.
-            if (Vector3.Dot(collision.GetContact(0).normal, impulse) < 0f)
-            {
-                impulse *= -1f;                
-            }
+            // Find direction between entities
+            Vector3 directionToHitMarble = (hitBody.transform.position - primaryBody.transform.position).normalized;
 
-            bVelocity = B.linearVelocity + (impulse / B.mass);
-            aVelocity = A.linearVelocity - (impulse / A.mass);
+            // Find how aligned the current velocity is against the direction to hit the other marble
+            float velocityAlignment = Vector3.Dot(velocity.normalized, directionToHitMarble);
+
+            // Scale damage by how much the current velocity aligns with the hit marble:
+            float alignedVelocity = velocity.magnitude * velocityAlignment;
+
+            return alignedVelocity;
         }
 
-        private static float FindDamageScaling(float playerVelocity, float enemyVelocity)
+
+
+        private float FindRawDamageForCollision(Marble marble, float mass, float alignedVelocity)
         {
-            float damageScaling;
+            // Base damage is simply mass * velocity
+            float baseDamage = alignedVelocity * mass;
+            
+            // Vector3 position = marble.transform.position;
+            // Vector3 velocity = marble.cachedVelocity;
+            // Debug.DrawLine(position, position + velocity, Color.blue, 3f);
+            // Debug.Log($"Collision!   base damage:{baseDamage:0.0}, alignment: {velocityAlignment:0.0}, scaled: {scaledDamage:0.0}");
+            
+            return baseDamage;
+        }
+    
+
+        private static float FindDamageRampup(float playerVelocity, float enemyVelocity)
+        {
+            float damageRampup;
+            
             if (playerVelocity > enemyVelocity)
             {
-                damageScaling = playerVelocity / enemyVelocity;
+                damageRampup = Mathf.Clamp(playerVelocity / enemyVelocity, 0, 2f);
+                damageRampup = damageRampup / 2f;
             }
             else
             {
-                damageScaling = -(enemyVelocity / playerVelocity);
+                damageRampup = Mathf.Clamp(enemyVelocity / playerVelocity, 0f, 2f);
+                damageRampup = damageRampup / 2f;
+                damageRampup = -damageRampup;
             }
-            damageScaling = Mathf.Clamp(damageScaling, -1f, 1f);
-            return damageScaling;
+
+            return damageRampup;
         }
 
-        private float FindDamageForCollision(Rigidbody rb, Vector3 directionToTarget, float mass, out float scaledVelocity)
-        {
-            Vector3 velocity = rb.linearVelocity;
-            Vector3 position = rb.transform.position;
-
-            // Base damage is simply mass * velocity
-            float baseDamage = velocity.magnitude * mass;
-
-            // Scale damage by how much the current velocity aligns with the hit marble:
-            float velocityAlignment = Vector3.Dot(velocity.normalized, directionToTarget);
-            scaledVelocity = velocity.magnitude * velocityAlignment;
-            float scaledDamage = baseDamage * velocityAlignment;
-
-            Debug.DrawLine(position, position + velocity, Color.blue, 3f);
-            Debug.DrawLine(position, position + directionToTarget, Color.red, 3f);
-            // Debug.Log($"Collision!   base damage:{baseDamage:0.0}, alignment: {velocityAlignment:0.0}, scaled: {scaledDamage:0.0}");
-            
-            return scaledDamage;
-        }
-    
     }
 
 
