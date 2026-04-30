a2f895faa97ff80920575de493d2c324061d73e4
Oliver Coates <oliver.gcoates@gmail.com>
2026-04-30 20:07:09 +1200

    Added the ability to walljump

--- Assets/MarbleBash/Player/Control/PlayerMovement.cs
+++ Assets/MarbleBash/Player/Control/PlayerMovement.cs
@@ -8,7 +8,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private PlayerLook _playerLook;
     private Rigidbody _rb;
 
-
     #region Input
     [Header("Input:")]
     [SerializeField] private InputActionAsset _inputActions;
@@ -23,13 +22,12 @@ public class PlayerMovement : MonoBehaviour
 
     #endregion
 
-
     [Header("Settings:")]
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
+    [SerializeField] private LayerMask _groundedLayerMask;
 
     [Header("State:")]
-    [SerializeField] private LayerMask _groundedLayerMask;
     [SerializeField] private bool _isGrounded;
     public bool isGrounded
     {
@@ -38,6 +36,25 @@ public class PlayerMovement : MonoBehaviour
             return _isGrounded;
         }
     }
+    
+    [SerializeField] private bool _isAgainstWall;
+    public bool isAgainstWall
+    {
+        get
+        {
+            return _isAgainstWall;
+        }	
+    }
+  
+    [SerializeField] private Vector3 _wallNormal;
+    public Vector3 wallNormal
+    {
+        get
+        {
+            return _wallNormal;
+        }	
+    }
+
     /// <summary>
     /// The position on the surface directly below the player. 
     /// </summary>
@@ -74,7 +91,6 @@ public class PlayerMovement : MonoBehaviour
 
         _isGrounded =  Physics.CheckBox(groundBoxPosition, goundBoxSize, Quaternion.identity, _groundedLayerMask);
         
-
         // Update our grounded position:
         Ray downRay = new Ray(transform.position, Vector3.down);
         if (Physics.Raycast(downRay, out RaycastHit hit, 100f, _groundedLayerMask))
@@ -82,6 +98,8 @@ public class PlayerMovement : MonoBehaviour
             _groundedPosition = hit.point;
         }
     }
+
+
 
     private void MoveHorizontally()
     {
@@ -98,10 +116,56 @@ public class PlayerMovement : MonoBehaviour
         {
             Jump();
         }
+        else if (_isAgainstWall)
+        {
+            WallJump();    
+        }
     }
 
     private void Jump()
     {
         _rb.AddForce(Vector3.up * _jumpForce, ForceMode.VelocityChange);
+    }
+
+    private void WallJump()
+    {
+        Vector3 direction = (_wallNormal + Vector3.up).normalized;
+        // Debug.DrawRay(transform.position, direction, Color.hotPink, 5f);
+
+        _rb.AddForce(direction * _jumpForce, ForceMode.VelocityChange);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (_isGrounded == false && IsObjectOnGroundedLayer(collision.gameObject))
+        {
+            if (IsCollisionAgainstWall(collision))
+            {
+                _isAgainstWall = true;        
+                _wallNormal = collision.contacts[0].normal;    
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if ((_groundedLayerMask.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            _isAgainstWall = false;
+        }
+    }
+
+    private bool IsObjectOnGroundedLayer(GameObject obj)
+    {
+        return ((_groundedLayerMask.value & (1 << obj.layer)) > 0); 
+    }
+
+    private bool IsCollisionAgainstWall(Collision collision)
+    {
+        // Find difference in height between the player and the point hit
+        float yDiff = Mathf.Abs(transform.position.y - collision.contacts[0].point.y);
+
+        // We consider against a wall if the difference in y is less than 0.2 units.
+        return yDiff < 0.2f; 
     }
 }
