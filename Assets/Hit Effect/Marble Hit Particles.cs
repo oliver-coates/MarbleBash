fea5f5cc2949dfb1d14ce0559032caa79b13ff56
Oliver Coates <oliver.gcoates@gmail.com>
2026-05-07 13:16:00 +1200

    Added hit shards which fly off marbles when hit

---
+++ Assets/Hit Effect/Marble Hit Particles.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+
+namespace MarbleBash
+{
+    
+    public class MarbleHitParticles : MonoBehaviour
+    {
+        private ParticleSystem _particles;
+
+        #region Initialisation & Destruction
+        private void Awake()
+        {
+            MarbleHealth.OnDamageTakenGlobal += MarbleTakenDamage;
+        
+            _particles = this.GetComponentSafe<ParticleSystem>();
+        }
+
+        private void OnDestroy()
+        {
+            MarbleHealth.OnDamageTakenGlobal -= MarbleTakenDamage;            
+        }
+        
+        #endregion
+
+
+        private void MarbleTakenDamage(MarbleHealth.HealthChangedEvent @event)
+        {
+            Vector3 location = @event.marble.transform.position;
+            int amount = UnityEngine.Random.Range(0, 4) + Mathf.CeilToInt(-@event.totalHealthChange / 10);
+
+            Emit(amount, location);
+        }
+
+        private void Emit(int amount, Vector3 location)
+        {
+            transform.position = location;
+            _particles.Emit(amount);            
+        }
+
+
+
+    }
+}
